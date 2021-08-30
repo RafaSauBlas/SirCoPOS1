e36bd9396d6e841aa8fda0b5e25d2bbc126f08ac
luisgarcian <luisgarcian@hotmail.com>
2021-08-30 12:53:05 -0500

    Agregar Observaciones al ticket de devolucion por cada producto devuelto, segun campos idrazon, notas de tabla devoluciondet

---
+++ SirCoPOS.Reports/Entities/DevolObservacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirCoPOS.Reports.Entities
+{
+    public class DevolObservacion
+    {
+        public string Serie { get; set; }
+        public string Razon { get; set; }
+        public string Comentarios { get; set; }
+    }
+}
